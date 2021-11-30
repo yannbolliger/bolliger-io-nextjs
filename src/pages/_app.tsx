6210b66b19e8f232970dcda6862e50caf942e33e
Yann Bolliger <yann@bolliger.io>
2021-11-30 16:25:32 +0100

    Fix favicon paths.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,24 +15,24 @@ const App = ({ Component, pageProps }: AppProps) => (
       <link
         rel="apple-touch-icon"
         sizes="180x180"
-        href="static/favicon/apple-touch-icon.png"
+        href="favicon/apple-touch-icon.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="32x32"
-        href="static/favicon/favicon-32x32.png"
+        href="favicon/favicon-32x32.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="16x16"
-        href="static/favicon/favicon-16x16.png"
+        href="favicon/favicon-16x16.png"
       />
-      <link rel="manifest" href="static/favicon/site.webmanifest" />
+      <link rel="manifest" href="favicon/site.webmanifest" />
       <link
         rel="mask-icon"
-        href="static/favicon/safari-pinned-tab.svg"
+        href="favicon/safari-pinned-tab.svg"
         color={colors.primary}
       />
       <meta name="apple-mobile-web-app-title" content="Bolliger Studios" />
