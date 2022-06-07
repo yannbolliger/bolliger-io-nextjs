09866c2b1440f5029db19c1298502e09a9a317f2
yannbolliger <yann@bolliger.io>
2022-06-07 17:09:29 +0000

    Deploying to gh-pages from @ yannbolliger/bolliger-io-nextjs@59776f9136aa3b15febe50207c693c193b137eb1 🚀

---
+++ _next/static/SeM9ifJb2xXWdm2El08UO/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/28-f7c2d4179a32ae15.js","static/chunks/pages/index-016139300f09d197.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
