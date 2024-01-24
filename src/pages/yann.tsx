b50766b6e6a5d4f798e5fc651fc89784dc4349bf
Yann Bolliger <yann@bolliger.io>
2024-01-24 16:38:18 +0100

    Add first draft of yann page without linking.

---
+++ src/pages/yann.tsx
@@ -0,0 +1,76 @@
+import Head from "next/head"
+import Image from "next/image"
+
+import LogoMenuHeader from "../components/LogoMenuHeader"
+import ProjectsSection from "../components/ProjectsSection"
+import TextBlockSection from "../components/TextBlockSection"
+import partners from "../content/partners"
+import { colors } from "../styled"
+import Img from "../styled/Img"
+import ItemContainer from "../styled/ItemContainer"
+import LinkList from "../styled/LinkList"
+import SplitView from "../styled/SplitView"
+import { LargeTitle, MediumParagraph } from "../styled/typography"
+
+const yannData = partners["yann"]
+const sections = [
+  {
+    id: "about",
+    title: "About me",
+  },
+  { id: "blog", title: "Blog" },
+  {
+    id: "projects",
+    title: "Projects",
+  },
+  {
+    id: "contact",
+    title: "Contact me",
+  },
+]
+
+const YannPage = () => (
+  <>
+    <Head>
+      <title>Yann Bolliger</title>
+    </Head>
+
+    <LogoMenuHeader
+      logoWords={[yannData.firstName, "Bolliger"]}
+      entries={[...sections, { href: "/", title: "Home" }]}
+    />
+
+    <TextBlockSection {...sections[0]}>
+      <Img as={Image} src={yannData.img} alt="Portrait" />
+      {yannData.about}
+    </TextBlockSection>
+
+    <TextBlockSection borderTop color={colors.primaryLight} title="Links">
+      <MediumParagraph as="div">
+        <LinkList entries={yannData.links} />
+      </MediumParagraph>
+    </TextBlockSection>
+
+    <TextBlockSection {...sections[1]} borderTop>
+      <LargeTitle>Thoughts</LargeTitle>
+      <MediumParagraph>Sometimes I write some stuff...</MediumParagraph>
+    </TextBlockSection>
+    <SplitView>
+      <ItemContainer
+        color={colors.primaryLight}
+        borderRight
+        title="Manual work is a Bug"
+        sideTitle="Organisation"
+      />
+      <ItemContainer
+        color={colors.primaryLight}
+        title="Manchmal auch etwas auf Deutsch mit einem typisch langen Titel"
+        sideTitle="Tech"
+      />
+    </SplitView>
+
+    <ProjectsSection {...sections[2]} />
+  </>
+)
+
+export default YannPage
